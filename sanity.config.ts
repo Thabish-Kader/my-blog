916e0043fb7f5895b17480f4f4c5c7fb8c74bee8
Thabish <kadertabish2@gmail.com>
2022-12-20 15:26:30 +0400

    sanity
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ sanity.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+
+export default defineConfig({
+	basePath: "/studio",
+	name: "MY_CONTENT_STUDIO",
+	title: "MY CONTENT STUDIO",
+
+	projectId,
+	dataset,
+
+	plugins: [deskTool(), visionTool()],
+
+	schema: {
+		types: schemaTypes,
+	},
+});
